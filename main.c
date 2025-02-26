86977b7a2ff35772320ad2622456639ad38a8406
Shresth Prasad <shresthprasad7@gmail.com>
2025-02-27 02:09:17 +0530

    add code to render initial window

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "raylib/raylib.h"
 #include "clip.h"
+#include <stdint.h>
+#include <stdlib.h>
 
 #define CLAY_IMPLEMENTATION
 #include "clay.h"
@@ -7,12 +9,42 @@
 
 #include <stdio.h>
 
+const float ScreenWidth = 1408;
+const float ScreenHeight = 792;
+
+void HandleClayErrors(Clay_ErrorData errorData) {
+    printf("%s", errorData.errorText.chars);
+}
+
 int main(void) {
-    Sel sel = CLIPBOARD;
-    char *out = retrieve_selection(sel);
+    Clay_Raylib_Initialize(ScreenWidth, ScreenHeight, "Simple Clipboard Manager", 0);
 
-    puts(out);
+    uint64_t clay_mem_size = Clay_MinMemorySize();
+    Clay_Arena mem_arena = {
+        .memory = malloc(clay_mem_size),
+        .capacity = clay_mem_size
+    };
 
-    return 0;
+    Clay_Dimensions dimension = {
+        .width = ScreenWidth,
+        .height = ScreenHeight
+    };
+
+    Clay_Initialize(mem_arena, dimension, (Clay_ErrorHandler) { HandleClayErrors });
+    SetTargetFPS(30);
+
+    // main app loop
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(YELLOW);
+
+        EndDrawing();
+    }
+    /*Sel sel = CLIPBOARD;*/
+    /*char *out = retrieve_selection(sel);*/
+
+    /*puts(out);*/
+
+    /*return 0*/;
 }
 
